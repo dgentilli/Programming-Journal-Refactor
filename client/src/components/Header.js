607f6b7ab10f628505a06a445cffaa9c06cc2122
dgentilli <dgentilli@gmail.com>
2020-03-13 14:37:05 +0200

    Header: Implement background image.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,4 @@
 import React, { Component } from "react";
-import styled from 'styled-components';
-
-const HeaderContainer = styled.div`
-    min-height: 450px;
-    color: #fff;
-`;
 
 class Header extends Component {
     constructor(props) {
@@ -17,8 +11,8 @@ class Header extends Component {
     render() {
         return (
             <div className="header-container">
-                <div class="bg-image"></div>
-                <div class="header-text-wrap">
+                <div className="bg-image"></div>
+                <div className="header-text-container">
                     <h1>Daily Programming Journal</h1>
                     <p>Every programmer needs a way to record lessons learned and document challenges encountered. This is the perfect way to chart your progress.</p>
                     <a href="#" class="btn">Get Started</a>
