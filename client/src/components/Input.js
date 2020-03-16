eca402ea1a28cb3907fe9aabeef41845f7afc5aa
dgentilli <dgentilli@gmail.com>
2020-03-16 09:18:24 +0200

    Input: Create a post from the front end.

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,14 +1,45 @@
 import React, { Component } from "react";
+import { json } from "body-parser";
 
 class Input extends Component {
     constructor(props) {
         super(props);
         this.state = {
             isLoaded: false,
-            topic: '',
-            lesson: ''
+            title: '',
+            content: ''
         };
     }
+
+    handleInputChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+        console.log("Input Component State: ", this.state);
+    }
+
+    submitData() {
+        console.log("Submit Data");
+        const journalData = {
+            title: this.state.title,
+            content: this.state.content
+        };
+
+        fetch('/api/journal/create', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(journalData)
+        }).then(res => res.json()).then(data => console.log("Journal Data sent to server: ", data));
+
+    }
+
+    handleSubmit = async e => {
+        console.log("Handle Submit Click");
+        e.preventDefault();
+        await this.submitData();
+        this.setState({ title: '', content: '' });
+    }
+
 
     render() {
         return (
@@ -17,18 +48,21 @@ class Input extends Component {
                 <form>
                     <input
                         type='text'
-                        name='topic'
+                        name='title'
                         placeholder="What sort of lesson or challenge are you writing about?"
-                        value={this.state.topic}
+                        value={this.state.title}
+                        onChange={this.handleInputChange}
                     />
                     <textarea
                         type='text'
-                        name='lesson'
+                        name='content'
                         placeholder="What do you want to record about that lesson learned or challenge encountered today?"
-                        value={this.state.lesson}
+                        value={this.state.content}
+                        onChange={this.handleInputChange}
+
                     />
                 </form>
-                <button id="submit-btn" className="form-btn">Submit</button>
+                <button id="submit-btn" className="form-btn" onClick={this.handleSubmit}>Submit</button>
                 <button id='cancel-btn' className='form-btn'>Cancel</button>
             </div>
         );
