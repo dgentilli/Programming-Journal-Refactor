9c355814246ca34ce92875947903659a81b9237d
dgentilli <dgentilli@gmail.com>
2020-03-17 11:05:09 +0200

    About: Add more text.

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -13,6 +13,7 @@ class About extends Component {
             <div id="about" className="about-container">
                 <h2>Contiuous Learning</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto sunt sapiente nihil nesciunt accusamus quibusdam vitae labore ut saepe? Facere debitis quod suscipit ratione, dignissimos nesciunt sapiente culpa pariatur tenetur laborum dolorem molestiae nihil voluptate quidem deleniti repellat ad. Ipsam?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate a totam vitae ullam molestias. Repudiandae temporibus distinctio dolorem amet delectus.</p>
             </div>
         );
     }
