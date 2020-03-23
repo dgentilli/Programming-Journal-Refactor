264ef71e239b2d27012f0ad68cdd37705c2aa620
dgentilli <dgentilli@gmail.com>
2020-03-23 11:27:00 +0200

    Footer: restyle portfolio link and open in new tab.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -17,7 +17,7 @@ class Footer extends Component {
                     <a href="#journal">Journal List</a>
                     <a href="#logout-btn">Logout</a>
                 </div>
-                <p className="footer-projects">Check out more of my projects <a href="https://www.davegentilli.com/">here</a></p>
+                <p className="footer-projects">Check out more of my projects <a href="https://www.davegentilli.com/" target="_blank">here</a></p>
                 <div className="copyright-container">Copyright &copy; {new Date().getFullYear()} Dave Gentilli</div>
             </div>
         );
