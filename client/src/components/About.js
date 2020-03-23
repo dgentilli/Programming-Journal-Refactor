d094139bba014fa1a3288635cac86098a897284e
dgentilli <dgentilli@gmail.com>
2020-03-23 10:48:52 +0200

    About: Add web copy, adjust styling.

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -11,9 +11,10 @@ class About extends Component {
     render() {
         return (
             <div id="about" className="about-container">
-                <h2>Contiuous Learning</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto sunt sapiente nihil nesciunt accusamus quibusdam vitae labore ut saepe? Facere debitis quod suscipit ratione, dignissimos nesciunt sapiente culpa pariatur tenetur laborum dolorem molestiae nihil voluptate quidem deleniti repellat ad. Ipsam?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate a totam vitae ullam molestias. Repudiandae temporibus distinctio dolorem amet delectus.</p>
+                <h2>Continuous Learning</h2>
+                <p>Keeping a journal is important in almost any endeavor, but it's especially necessary as a programmer. When I first started teaching myself to code, I was overwhelmed by how much there was to learn. There were days when I figured out how to overcome some challenge, only to find myself scratching my head a few days later, trying to remember how I solved that problem. Having a journal to reference prevents this problem, not to mention that the simple act of writing something helps us commit it to memory.</p>
+                <p>Another important reason to keep a journal as a programmer is to remind yourself how far you've come! That's right. One of the ways I overcame the feeling of being overwhelmed by how much there is to learn was to constantly remind myself how much I had learned already. It kept me on course and served as a reminder that this journey would move forward one step at a time.</p>
+                <p>Try out the app! If you like the project, you can see more of my work <a href="https://www.davegentilli.com/">here.</a></p>
             </div>
         );
     }
