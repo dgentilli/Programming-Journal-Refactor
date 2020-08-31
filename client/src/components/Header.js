aea24f5bb3753a1a180342f4b11c912f066ab93f
dgentilli <dgentilli@gmail.com>
2020-08-31 11:07:30 +0300

    Header: Refactor to function component.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,28 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Header extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoaded: false
-        };
-    }
-
-    render() {
-        return (
-            <div id="top" className="header-container">
-                <div className="bg-image"></div>
-                <div className="header-text-container">
-                    <h1>Daily Programming Journal</h1>
-                    <p>Every programmer needs a way to record lessons learned and document challenges encountered. This is the perfect way to chart your progress.</p>
-                    <div className="btn-container">
-                        <a href="#login-control" class="btn">Get Started</a>
-                        <a href="#about" class="btn">Read More</a>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
+const Header = () => {
+  return (
+    <div id="top" className="header-container">
+      <div className="bg-image"></div>
+      <div className="header-text-container">
+        <h1>Daily Programming Journal</h1>
+        <p>
+          Every programmer needs a way to record lessons learned and document
+          challenges encountered. This is the perfect way to chart your
+          progress.
+        </p>
+        <div className="btn-container">
+          <a href="#login-control" class="btn">
+            Get Started
+          </a>
+          <a href="#about" class="btn">
+            Read More
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
