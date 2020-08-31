0a93d7d790e5b94bc8581c91e94a3ea10359b232
dgentilli <dgentilli@gmail.com>
2020-08-31 11:04:43 +0300

    Footer: Refactor to function component.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,56 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Footer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoaded: false,
-      isLoggedIn: this.props.isLoggedIn,
-    };
-  }
+const Footer = ({ isLoggedIn }) => {
+  console.log("footer props", isLoggedIn);
 
-  componentDidUpdate(prevProps) {
-    if (this.props.isLoggedIn !== prevProps.isLoggedIn) {
-      this.setState({ isLoggedIn: !this.state.isLoggedIn });
-    }
-  }
+  const inputLink = isLoggedIn ? "#input" : "#login-control";
+  const journalLink = isLoggedIn ? "#journal" : "#login-control";
 
-  inputLink() {
-    if (this.state.isLoggedIn) {
-      return "#input";
-    } else {
-      return "#login-control";
-    }
-  }
-
-  journalLink() {
-    if (this.state.isLoggedIn) {
-      return "#journal";
-    } else {
-      return "#login-control";
-    }
-  }
-
-  render() {
-    return (
-      <div className="footer-container">
-        <div className="footer-nav-links">
-          <a href="#top">Go to Top</a>
-          <a href={this.inputLink()}>Create</a>
-          <a href={this.journalLink()}>Journal List</a>
-        </div>
-        <p className="footer-projects">
-          Check out more of my projects{" "}
-          <a href="https://www.davegentilli.com/" target="_blank">
-            here
-          </a>
-        </p>
-        <div className="copyright-container">
-          Copyright &copy; {new Date().getFullYear()} Dave Gentilli
-        </div>
+  return (
+    <div className="footer-container">
+      <div className="footer-nav-links">
+        <a href="#top">Go to Top</a>
+        <a href={inputLink}>Create</a>
+        <a href={journalLink}>Journal List</a>
       </div>
-    );
-  }
-}
+      <p className="footer-projects">
+        Check out more of my projects{" "}
+        <a href="https://www.davegentilli.com/" target="_blank">
+          here
+        </a>
+      </p>
+      <div className="copyright-container">
+        Copyright &copy; {new Date().getFullYear()} Dave Gentilli
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
