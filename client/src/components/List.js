905cc165f1a5ac0d1d8016af06c4025816db7026
dgentilli <dgentilli@gmail.com>
2020-08-31 12:19:34 +0300

    List: Refactor to function component.

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,74 +1,68 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 
-class List extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoaded: false,
-            journalEntries: [],
-            isLoggedIn: this.props.isLoggedIn,
-            user: this.props.user,
-            title: this.props.title,
-            journalContentShow: null
-        };
-    }
+const List = ({ isLoggedIn, user, title }) => {
+  const [journalEntries, setJournalEntries] = useState([]);
+  const [journalContentShow, setjournalContentShow] = useState(null);
 
-    componentDidMount() {
-        this.fetchJournalEntries();
-    }
+  useEffect(() => {
+    fetchJournalEntries();
+  }, []);
 
-    componentDidUpdate(prevProps) {
-        if (this.props.title !== prevProps.title) {
-            this.fetchJournalEntries();
-        }
-    }
+  useEffect(() => {
+    fetchJournalEntries();
+  }, [title]);
 
-    fetchJournalEntries() {
-        fetch(`/api/journal/all/${this.state.user.id}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }).then(res => {
-            const resBody = res.json();
-            Promise.resolve(resBody).then(journals => {
-                this.setState({
-                    journalEntries: journals
-                })
-            })
-        })
+  const fetchJournalEntries = () => {
+    fetch(`/api/journal/all/${user.id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      const resBody = res.json();
+      Promise.resolve(resBody).then((journals) => {
+        setJournalEntries(journals);
+      });
+    });
+  };
 
-    }
+  let journals = journalEntries;
+  let journalList;
+  if (journals.length > 0) {
+    journalList = journals.map((journal) => (
+      <li key={journal._id}>
+        <div>
+          <h4 className="journal-title">{journal.title}</h4>
+          <p className="journal-date">{journal.createdAt.substring(0, 10)}</p>
+          <p className="journal-content">
+            {journalContentShow !== journal._id
+              ? journal.content.substring(0, 100) + " ..."
+              : journal.content}
+          </p>
+          <div>
+            {journal._id !== journalContentShow ? (
+              <button onClick={() => setjournalContentShow(journal._id)}>
+                Read More
+              </button>
+            ) : (
+              <button onClick={() => setjournalContentShow(null)}>
+                Show Less
+              </button>
+            )}
+          </div>
+        </div>
+      </li>
+    ));
+  } else {
+    journalList = <li>There are no journal entries to display.</li>;
+  }
 
-    render() {
-        let journals = this.state.journalEntries;
-        let journalList;
-        if (journals.length > 0) {
-            journalList = journals.map(journal => (
-                <li key={journal._id}>
-                    <div>
-                        <h4 className='journal-title'>{journal.title}</h4>
-                        <p className="journal-date">{journal.createdAt.substring(0, 10)}</p>
-                        <p className='journal-content'>{this.state.journalContentShow !== journal._id ? journal.content.substring(0, 100) + " ..." : journal.content}</p>
-                        <div>{journal._id !== this.state.journalContentShow ? <button onClick={() => this.setState({ journalContentShow: journal._id })}>Read More</button> : <button onClick={() => this.setState({ journalContentShow: null })}>Show Less</button>}</div>
-                    </div>
-                </li>
-            ))
-        } else {
-            journalList = <li>There are no journal entries to display.</li>
-        }
-
-
-        return (
-            <div id="journal" className="list-container">
-                <h2>Journal Entries for {this.state.user.email}</h2>
-                <ul>
-                    {journalList}
-                </ul>
-
-            </div>
-        );
-    }
-}
+  return (
+    <div id="journal" className="list-container">
+      <h2>Journal Entries for {user.email}</h2>
+      <ul>{journalList}</ul>
+    </div>
+  );
+};
 
 export default List;
