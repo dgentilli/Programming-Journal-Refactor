b4aba3f1323dbff2362fa59d91590affd2e7d498
dgentilli <dgentilli@gmail.com>
2020-08-31 17:55:04 +0300

    LoginControl: Refactor Logout

--- client/src/components/LoginControl.js
+++ client/src/components/LoginControl.js
@@ -4,6 +4,7 @@ import Footer from "./Footer";
 import About from "./About";
 import Login from "./Login";
 import Signup from "./Signup";
+import Logout from "./Logout";
 import { useChangeHandler } from "../hooks/useChangeHandler";
 
 const LoginControl = () => {
@@ -95,21 +96,7 @@ const LoginControl = () => {
       />
     );
   } else if (isLoggedIn) {
-    loginSignup = (
-      <div id="login-control" className="login-signup-container">
-        <div className="logout-btn-container">
-          <button id="logout-btn" onClick={logoutClick}>
-            Logout
-          </button>
-          <button id="logout-btn">
-            <a href="#journal">Journal List</a>
-          </button>
-          <button id="logout-btn">
-            <a href="#about">About</a>
-          </button>
-        </div>
-      </div>
-    );
+    loginSignup = <Logout handleLogout={logoutClick} />;
   }
 
   let display = isLoggedIn ? (
---
+++ client/src/components/Logout.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const Logout = ({ handleLogout }) => {
+  return (
+    <div id="login-control" className="login-signup-container">
+      <div className="logout-btn-container">
+        <button id="logout-btn" onClick={handleLogout}>
+          Logout
+        </button>
+        <button id="logout-btn">
+          <a href="#journal">Journal List</a>
+        </button>
+        <button id="logout-btn">
+          <a href="#about">About</a>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Logout;
