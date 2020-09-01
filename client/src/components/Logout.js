00ee382f4d0e32a1802a3ece6a4a7256292f3aea
dgentilli <dgentilli@gmail.com>
2020-09-01 16:31:43 +0300

    WIP: Implement smooth scrolling for menu bar displayed when logged in.

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,18 +1,44 @@
 import React from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 const Logout = ({ handleLogout }) => {
   return (
     <div id="login-control" className="login-signup-container">
       <div className="logout-btn-container">
-        <button id="logout-btn" onClick={handleLogout}>
+        {/* <button className="logout-btn" onClick={handleLogout}>
           Logout
-        </button>
-        <button id="logout-btn">
+        </button> */}
+        <Link onClick={handleLogout} className="logout-btn">
+          Logout
+        </Link>
+        {/* <button id="logout-btn">
           <a href="#journal">Journal List</a>
-        </button>
-        <button id="logout-btn">
+        </button> */}
+        <Link
+          className="logout-btn"
+          activeClass="active"
+          to="journal"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+        >
+          Journal List
+        </Link>
+        {/* <button id="logout-btn">
           <a href="#about">About</a>
-        </button>
+        </button> */}
+        <Link
+          className="logout-btn"
+          activeClass="active"
+          to="about"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+        >
+          About
+        </Link>
       </div>
     </div>
   );
